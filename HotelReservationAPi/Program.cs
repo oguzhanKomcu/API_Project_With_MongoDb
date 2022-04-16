a20ab4b2ca916b7d59671617f0d664389932a8d9
oğuzhan kömcü <komcuoguzz@gmail.com>
2022-04-16 15:46:57 +0300

    AddSwaggerGen()  required coding done.

--- HotelReservationAPi/Program.cs
+++ HotelReservationAPi/Program.cs
@@ -1,5 +1,8 @@
 using HotelReservationAPi.Infrastructure.Repositories;
 using HotelReservationAPi.Model;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.Swagger;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +13,31 @@ builder.Services.AddScoped<IReservationRepository, ReservationRepository>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("RestAPI", new OpenApiInfo()
+    {
+        Title = "RestFul API",
+        Version = "v1",
+        Description = "Hotel Reservation RestFul API",
+        Contact = new OpenApiContact()
+        {
+            Email = "komcuoguzz@gmail.com",
+            Name = "Oðuzhan Kömcü",
+            Url = new Uri("https://github.com/oguzhanKomcu")
+        },
+        License = new OpenApiLicense()
+        {
+            Name = "MIT License",
+            Url = new Uri("http://opensource.org/licenses/MIT")
+        }
+    });
+
+   
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
+
 
 var app = builder.Build();
 
